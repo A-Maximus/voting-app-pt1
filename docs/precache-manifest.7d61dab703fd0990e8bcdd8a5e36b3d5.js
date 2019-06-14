1234d1043fc691a30f7e058fc4fb0bcae4fa7c07
A-Maximus <awachuku86@gmail.com>
2019-06-14 16:23:11 -0400

    made a voting app

---
+++ docs/precache-manifest.7d61dab703fd0990e8bcdd8a5e36b3d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3724dc6c0b26829661cf25b7f451a485",
+    "url": "/voting-app-pt1/index.html"
+  },
+  {
+    "revision": "65add61d31fc70f080b9",
+    "url": "/voting-app-pt1/static/css/main.23a10b6b.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/voting-app-pt1/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "65add61d31fc70f080b9",
+    "url": "/voting-app-pt1/static/js/main.1de02990.chunk.js"
+  },
+  {
+    "revision": "ededc37e615d28b9459d",
+    "url": "/voting-app-pt1/static/js/runtime~main.237331f4.js"
+  }
+]);
