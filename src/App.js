cc7ee1a94e8b4df5849311a9fb3478cf85e716dd
A-Maximus <awachuku86@gmail.com>
2019-06-14 12:37:11 -0400

    created voting app pt1

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+// import { plus-sign } from './src/plus-sign.png';
+
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      js_library: [
+        {name: "React", votes: 0},
+        {name: "Vue", votes: 0},
+        {name: "Angular", votes: 0},
+        {name: "Ember", votes: 0}
+      ]
+    };
+  }
+
+  vote(i) {
+    console.log("voting has occured", i);
+    let j = [...this.state.js_library];
+    j[i].votes++;
+    this.setState({js_library: j});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Vote Your JS Library!</h1>
+        {
+          this.state.js_library.map((j, i) =>
+          <div key ={i}className= "js_library">
+            <h4>{j.name}</h4>
+            <button onClick={this.vote.bind(this, i)}>Likes: {j.votes}
+            </button>
+            {/* <img src={src/plus-sign.png} alt="plus sign"></img> */}
+          </div>
+          )
+        }
+      </div>
+    );
+
+  }
+}
+
+export default App;
